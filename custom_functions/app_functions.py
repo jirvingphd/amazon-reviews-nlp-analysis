8001343f382dfed59ae944ee88e3f7448a8dafa4
James M. Irving, Ph.D <james.irving.phd@gmail.com>
2024-02-20 18:12:04 -0500

    Updated EDA app with download buttons

--- custom_functions/app_functions.py
+++ custom_functions/app_functions.py
@@ -1,6 +1,18 @@
 # @st.cache_data
+def load_filepaths_json(fname="config/filepaths.json"):
+    ##Load in the data
+    import json
+    with open(fname) as f:
+        FPATHS = json.load(f)
+    print("Top-Level Keys in FPATHS dict:")
+    # [print(f'- {k}') for k in FPATHS.keys()]
+    print(FPATHS.keys())
+    return FPATHS
 
+
+FPATHS = load_filepaths_json()
 ## Adding caching to reduce api usage
+import os
 from langchain.cache import InMemoryCache
 # from langchain.document_loaders import CSVLoader
 from langchain_community.document_loaders import CSVLoader
@@ -12,20 +24,135 @@ from langchain.prompts import (
     MessagesPlaceholder,
     SystemMessagePromptTemplate,
 )
-from langchain.text_splitter import CharacterTextSplitter#, SpacyTextSplitter
-from langchain_community.vectorstores import FAISS, Chroma
-from langchain_openai.chat_models import ChatOpenAI
+import streamlit as st
+import custom_functions as fn
 from langchain_openai.embeddings import OpenAIEmbeddings
-# from langchain_community.embeddings.spacy_embeddings import SpacyEmbeddings
+from langchain_community.vectorstores import Chroma, FAISS
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.callbacks import StreamlitCallbackHandler
+from langchain.callbacks import StdOutCallbackHandler
+from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain_openai.chat_models import ChatOpenAI
+from langchain.schema import SystemMessage, HumanMessage, AIMessage
+from langchain.prompts import MessagesPlaceholder
+from langchain.tools.retriever import create_retriever_tool
+from langchain_community.document_loaders import CSVLoader
 
+from langchain.agents import AgentExecutor, create_openai_tools_agent
+# Memory: agent token buffer used in original example blog post
+from langchain.agents.openai_functions_agent.agent_token_buffer_memory import AgentTokenBufferMemory
+from langchain.memory import ConversationBufferMemory
 # set_llm_cache(InMemoryCache())
 
 from langchain import hub
 from langchain.agents import AgentExecutor, create_openai_tools_agent
 from langchain.tools.retriever import create_retriever_tool
 
+
+def get_template_string_reviews(FPATHS):
+     # Create template with product info
+    template = f"You are a helpful data analyst for answering questions about what customers said about a specific  Amazon product using only content from use reviews."
+    from custom_functions.app_functions import load_product_info
+    product_string = load_product_info(FPATHS['data']['app']['product-metadata-llm_json'])
+
+    product_template = f" Assume all user questions are asking about the content in the user reviews. Note the product metadata is:\n```{product_string}```\n\n"
+    template+=product_template
+    
+    qa_prompt_template ="""Use the following pieces of context (user reviews) to answer the user's question by summarizing the reviews. 
+            If you don't know the answer, just say that you don't know, don't try to make up an answer.\n----------------\n{agent_scratchpad}\n\n"""
+    template+=qa_prompt_template
+    return template
+    
+    
+
+
+def get_agent(retriever=None,fpath_db=FPATHS['data']['app']['vector-db_dir'],
+              ## Updated function
+              fpath_csv = FPATHS['data']['app']['reviews-with-target-for-llm_csv'],
+              k=8, temperature=0.1, verbose=False,
+             template_string_func=get_template_string_reviews):
+    
+    ## Make retreieval tool
+    if retriever is None:
+        retriever  = load_vector_database( fpath_db,fpath_csv, k=k, use_previous=True, as_retriever=True)#, use_previous=False)
+    tool = create_retriever_tool(
+        retriever,
+        "search_reviews",
+        "Searches and returns excerpts from Amazon user reviews.",
+    )
+    tools = [tool]
+    
+    
+   ## Get template via function for template string
+    template = template_string_func()
+
+
+    # Create the chatprompttemplate
+    prompt_template = OpenAIFunctionsAgent.create_prompt(
+        system_message=SystemMessage(template),
+        extra_prompt_messages=[MessagesPlaceholder(variable_name="history")],
+    )
+    
+    if verbose:
+        print(prompt_template.messages)
+        
+    llm = ChatOpenAI(temperature=temperature,streaming=True, api_key=os.getenv("OPENAI_API_KEY"))
+    agent = create_openai_tools_agent(llm, tools, prompt_template)
+    
+    ## Creating streamlit-friendly memory for streaming
+    agent_executor = AgentExecutor(agent=agent, tools=tools,  verbose=True, #return_intermediate_steps=True,
+                                   memory=ConversationBufferMemory(memory_key="history",return_messages=True))
+    return agent_executor
+
+
+            
+            
+def reset_agent(#fpath_db = FPATHS['data']['app']['vector-db_dir'],
+                retriever=None,
+                ai_avatar="🤖", user_avatar = "💬",
+                starter_message = "Hello, there! Enter your question here and I will check the full reviews database to provide you the best answer.",
+               get_agent_kws={}):
+    # fpath_db
+    if retriever is None:
+        retriever = st.session_state['retriever']
+    agent_exec = get_agent(retriever, **get_agent_kws)
+    agent_exec.memory.chat_memory.add_ai_message(starter_message)
+    # with chat_container:
+    st.chat_message("assistant", avatar=ai_avatar).write_stream(fake_streaming(starter_message))
+        # print_history(agent_exec)
+    return agent_exec
+    
+
+def fake_streaming(response):
+    import time
+    for word in response.split(" "):
+        yield word + " "
+        time.sleep(.05)		
+        
+            
+    
+## For steramlit try this as raw code, not a function
+def print_history(agent_executor, ai_avatar ="🤖", user_avatar="💬"):
+    # Simulate streaming for final message
+
+    session_state_messages = agent_executor.memory.buffer_as_messages
+    for msg in session_state_messages:#[:-1]:
+        if isinstance(msg, AIMessage):
+            # notebook
+            print(f"Assistant: {msg.content}")
+            # streamlit
+            st.chat_message("assistant", avatar=ai_avatar).write(msg.content)
+        
+        elif isinstance(msg, HumanMessage):
+            # notebook
+            print(f"User: {msg.content}")
+            # streamlit
+            st.chat_message("user", avatar=user_avatar).write(msg.content)
+        print()
+        
+
 def display_metadata(meta_df,iloc=0, include_details=False):
-    # product = meta_df.iloc[iloc]
+    product = meta_df.iloc[iloc]
     # md = "#### Product Being Reviewed"
     md = ""
     md += f'\n- Product Title:\n***\"{product["Title (Raw)"]}\"***'
@@ -57,24 +184,29 @@ def load_product_info(fpath):
     return product_string
 
 
+@st.cache_resource
 def load_vector_database(fpath_db, fpath_csv=None, metadata_columns = ['reviewerID'],
-                         chunk_size=500, use_previous = True,
-                         delete=False, as_retriever=False, k=8, **retriever_kwargs):
-    
+                         chunk_size=500, use_previous = False,
+                        #  delete=False, 
+                         as_retriever=False, k=8, **retriever_kwargs):
+    import os
      # Use EMbedding --> embed chunks --> vectors
-    embedding_func = OpenAIEmbeddings()
+    embedding_func = OpenAIEmbeddings(api_key=os.getenv('OPENAI_API_KEY'))
     
-    if delete==True:
-        # Set use_pervious to False
-        use_previous= False
-        db = Chroma(persist_directory=fpath_db, 
-           embedding_function=embedding_func)
-        db.delete_collection()
+    # if delete==True:
+    #     # print("Deleting previous Chroma db...")
+    #     # Set use_pervious to False
+    #     use_previous= False
+    #     # db = Chroma(persist_directory=fpath_db, 
+    #     #    embedding_function=embedding_func)
+    #     # db.delete_collection()
 
     if use_previous==True:
-        db =  Chroma(persist_directory=fpath_db, 
-           embedding_function=embedding_func)
+        print("Using previous vector db...")
+        db = FAISS.load_local(fpath_db, embedding_func)
+
     else:
+        print("Creating embeddings/Chromadb database")
         if fpath_csv == None:
             raise Exception("Must pass fpath_csv if use_previous==False or delete==True")
                 
@@ -85,14 +217,15 @@ def load_vector_database(fpath_db, fpath_csv=None, metadata_columns = ['reviewer
         text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=chunk_size)
         docs = text_splitter.split_documents(documents)
         
-        db = Chroma.from_documents(docs, embedding_func, persist_directory= fpath_db)
+        # db = Chroma.from_documents(docs, embedding_func, persist_directory= fpath_db)
+        db = FAISS.from_documents(docs, embedding_func)
         # Use persist to save to disk
-        db.persist()
+        # db.persist()
+        db.save_local(fpath_db)
 
     if as_retriever:
         return db.as_retriever(k=k, **retriever_kwargs)
     else:
         return db
-
     
     
--- pages/1_🖼️_NLP_EDA.py
+++ pages/1_🖼️_NLP_EDA.py
@@ -10,6 +10,29 @@ import custom_functions as fn
 import plotly.express as px
 import plotly.io as pio
 pio.templates.default='streamlit'
+
+import streamlit as st
+import custom_functions as fn
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma, FAISS
+from langchain.text_splitter import CharacterTextSplitter
+# from langchain_community.callbacks import StreamlitCallbackHandler
+from langchain.callbacks import StdOutCallbackHandler
+from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain_openai.chat_models import ChatOpenAI
+from langchain.schema import SystemMessage, HumanMessage, AIMessage
+from langchain.prompts import MessagesPlaceholder
+from langchain.tools.retriever import create_retriever_tool
+from langchain_community.document_loaders import CSVLoader
+
+# from langchain.agents.agent_toolkits import create_pandas_dataframe_agent
+
+
+from langchain.agents import AgentExecutor, create_openai_tools_agent
+# Memory: agent token buffer used in original example blog post
+from langchain.agents.openai_functions_agent.agent_token_buffer_memory import AgentTokenBufferMemory
+from langchain.memory import ConversationBufferMemory
+
 # Changing the Layout
 st.set_page_config( layout="wide", 
                    page_icon="⭐️Amazon Reviews NLP EDA")
@@ -30,7 +53,57 @@ import json
 with open("config/filepaths.json") as f:
     FPATHS = json.load(f)
     
+# FPATHS = fn.load_filepaths_json()
+fpath_llm_csv = FPATHS['data']['app']['reviews-with-target-for-llm_csv']
+fpath_db = FPATHS['data']['app']['vector-db_dir']
+
+        
+        
+
+if 'retriever' not in st.session_state:
+    # retriever  = load_vector_database( fpath_db,fpath_llm_csv, k=8, use_previous=False, as_retriever=True)    
+    if os.path.exists(fpath_db):
+        retriever = fn.load_vector_database(fpath_db, fpath_llm_csv, use_previous=True, as_retriever=True)
+    else:
+        retriever = fn.load_vector_database(fpath_db, fpath_llm_csv, use_previous=False, as_retriever=True)
     
+    st.session_state['retriever'] = retriever
+
+
+
+# Create chat container early
+    
+def display_metadata(meta_df,iloc=0, include_details=False):
+    # product = meta_df.iloc[iloc]
+    # md = "#### Product Being Reviewed"
+    md = ""
+    md += f'\n- Product Title:\n***\"{product["Title (Raw)"]}\"***'
+    # md += f"<p><img src='{product['Product Image']}' width=300px></p>"
+    md += f'\n- Brand: {product["Brand"]}'
+    md += f"\n- Price: {product['Price']}"
+    md += f"\n- Ranked {product['Rank']} (2018)"
+
+    md += f"\n- Categories:\n    - "
+    md += "; ".join(product['Categories'])
+    # md += 
+    # md += f"\n- Categories:{', '.join(product['Categories'])}"
+    
+    
+    return md
+
+
+def load_product_info(fpath):
+    import json
+    with open(fpath,'r') as f:
+        product_json = json.load(f)
+        
+    product_string = "Product Info:\n"
+    for k,v in product_json.items():
+        if k.lower()=='description':
+            continue
+        product_string+=f"\n{k} = {v}\n"
+        
+    return product_string
     
 ## Title /header
 # st.header("Exploratory Data Analysis of Amazon Reviews ")
@@ -42,7 +115,6 @@ st.header("NLP Exploratory Visualizations")
 ## Select which text column/preprocessing
 # st.sidebar.header("Display Options")
 # st.sidebar.markdown(">*Select visualizations for main pane.*")
-show_product= st.checkbox("Show Product Information", value=True)
 # show_review_graphs = st.sidebar.checkbox("Show rating distibutions.", value=True)
 # show_yearly =st.sidebar.checkbox('Show yearly trends in reviews', value=True)
 # show_wordclouds = st.sidebar.checkbox('Show Word Couds', value=True)
@@ -82,7 +154,8 @@ product= meta_df.iloc[0]
 # st.markdown("##### ***👈 Select the Display Options to enable/disable app components.***")
 # st.divider()
 # st.subheader("Exploratory Analysis ")
-
+st.divider()
+show_product= st.checkbox("Show Product Information", value=False)
 
 if show_product==True:
     st.subheader("Product Information")
@@ -120,18 +193,20 @@ else:
 
 
 
-
 st.sidebar.subheader("Text Preprocessing Options")
+settings_menu = st.sidebar.container(border=True)
+
 # with st.container(border=True):
-st.sidebar.markdown(">*Select form of text for NLP EDA visuals.*")
+settings_menu.markdown(">*Select form of text for NLP EDA visuals.*")
 
 # col1,col2=st.columns(2)
+
 
 text_col_map  ={"Original Text":'review-text-full',
                 "Tokenized Text (no stopwords)":'tokens',
             'Lemmatzied Text':'lemmas'
             }
-text_preprocessing_selection  =  st.sidebar.radio("Select Tokenization",options=list(text_col_map.keys()),# ['Original','Lemmas','Cleaned Tokens'],
+text_preprocessing_selection  =  settings_menu.radio("Select Tokenization",options=list(text_col_map.keys()),# ['Original','Lemmas','Cleaned Tokens'],
                                             index=0)
 text_col_selection = text_col_map[text_preprocessing_selection]
 ## Select # of words/ngrams
@@ -139,31 +214,18 @@ ngram_map = {'Unigrams/Tokens (1 Word)':1,
             'Bigrams (2 words)':2,
             'Trigrams (3 words)':3,
             'Quadgrams (4 words)':4}
-ngram_selection = st.sidebar.radio("Select ngrams", options=list(ngram_map.keys()), #['Single Words','Bigrams','Trigrams','Quadgrams'],
+
+
+ngram_selection = settings_menu.radio("Select ngrams", options=list(ngram_map.keys()), #['Single Words','Bigrams','Trigrams','Quadgrams'],
                         index=1)
 ngram_n = ngram_map[ngram_selection]
 # Select custom stopwords
-add_stopwords_str = st.sidebar.text_area("Enter list of words to exclude:",value='five,one,star,angel,hair,miracle,noodles,shirataki,pasta')
+add_stopwords_str = settings_menu.text_area("Enter list of words to exclude:",value='five,one,two,star,stars,angel,hair,miracle,noodles,shirataki,pasta')
 stopwords_list = fn.get_stopwords_from_string(add_stopwords_str)
 
 
 st.sidebar.divider()
 
-
-# st.sidebar.subheader("Dev Options")
-# dev_show_fpaths = st.sidebar.checkbox('[Dev] Show FPATHS?',value=False)
-# dev_show_frame = st.sidebar.checkbox("[Dev] Show frame?",value=False)
-
-# if dev_show_fpaths:
-#     FPATHS
-    
-# if dev_show_frame:
-#     st.dataframe(df.head())
-
-
-# st.divider()
-
-# st.image(FPATHS['images']['selected-product_jpg'])
 
 
 ## word clouds
@@ -181,61 +243,70 @@ def fn_get_groups_freqs_wordclouds(df,ngrams=ngram_n, as_freqs=True,
     kwargs = locals()
     group_texts = fn.get_groups_freqs_wordclouds(**kwargs) #testing stopwords
     return group_texts
-## MENU FOR WORDCLOUDS
+
+
+    
+def download_fig(fig,):
+    sname = 'word clodus'
+    # Source: https://stackoverflow.com/questions/71713951/how-to-download-matplotlib-graphs-generated-in-a-streamlit-app
+    import io
+    img = io.BytesIO()
+    fig.savefig(img, format='png', dpi=300,bbox_inches='tight' )
+    return img
+    # if submit_button:
+    
+# chat_options.markdown("**Clear conversation history.**")
+
+# reset_button  = chat_options.button("Clear",on_click=reset)
 
 
 
-
-
-    # wc_col1, wc_col2 = st.columns(2)
-    # text_preprocessing_selection  =  wc_col1.radio("Select Text Processing",options=list(text_col_map.keys()),# ['Original','Lemmas','Cleaned Tokens'],
-    #                                         index=0)
-    # text_col_selection = text_col_map[text_preprocessing_selection]
-
-    # ## Select # of words/ngrams
-    # ngram_map = {'Single Words':1,
-    #             'Bigrams':2,
-    #             'Trigrams':3,
-    #             'Quadgrams':4}
-    # ngram_selection = wc_cofl2.radio("Select ngrams", options=list(ngram_map.keys()), #['Single Words','Bigrams','Trigrams','Quadgrams'],
-    #                         index=0)
-    # ngram_n = ngram_map[ngram_selection]
-
-    # Select custom stopwords
-    # add_stopwords_str = wc_col1.text_input("Enter list of words to exclude:",value='five,one,star')
-    # stopwords_list = fn.get_stopwords_from_string(add_stopwords_str)
 with st.container(border=True):
-    st.markdown(">👈 *Change Text Preprocessing Options on the sidebar.*")
+    c1, c2,c3 =st.columns(3)
+    c1.markdown("#### Text Preprocesing")
+    c1.markdown(">👈 *Change Text Preprocessing Options on the sidebar.*")
+    
+    c1.markdown("- Recommended Settings:\n    - Tokenization = 'Original Text' \n    - ngrams=Bigrams/Trigrams")
     group_texts = fn_get_groups_freqs_wordclouds(df,ngrams=ngram_n, as_freqs=True,group_col='target-rating', text_col = text_col_selection,
                                         stopwords=stopwords_list )
 # preview_group_freqs(group_texts)
 
-    col1, col2 = st.columns(2)
-    min_font_size = col1.number_input("Minumum Font Size",min_value=4, max_value=50,value=6, step=1)
-    max_words = col2.number_input('Maximum # of Words', min_value=10, max_value=1000, value=200, step=5)
-
-fig  = fn.make_wordclouds_from_freqs(group_texts,stopwords=stopwords_list,min_font_size=min_font_size, max_words=max_words,
+    # col1, col2 = st.columns(2)
+    c2.markdown("##### WordCloud Options")
+    min_font_size = c2.number_input("Minumum Font Size",min_value=4, max_value=50,value=6, step=1)
+    max_words = c2.number_input('Maximum # of Words', min_value=10, max_value=1000, value=200, step=5)
+    fig  = fn.make_wordclouds_from_freqs(group_texts,stopwords=stopwords_list,min_font_size=min_font_size, max_words=max_words,
                                      figsize=(16,10))
+    c3.markdown('##### Download word cloud image.')
+    filename = c3.text_input("Filename (png)", value='wordcloud-comparison.png')
+    download_fig_button =c3.download_button("Download image.", data =download_fig(fig),file_name=filename,mime="image/png", )
 
-st.pyplot(fig)
+with st.container():
+    st.pyplot(fig)
 
 st.divider()
 
 
 ## Add creating ngrams
-st.subheader('N-Grams')
+st.subheader('N-Gram Bar Graphs')
 
-with st.container(border=True):
-
-    st.markdown(">👈 Change Text Preprocessing Options on the sidebar.")
+ngram_menu = st.container(border=True)
+with ngram_menu:
+    col1,col2,col3 = ngram_menu.columns(3)
+    col1.markdown("> 👈 *Change Text Preprocessing Options on the sidebar*")
+    col1.markdown("- Recommended Settings:\n    - Tokenization = 'Lemmatized Text'\n    - ngrams=Trigrams")
 
     # ngrams = st.radio('n-grams', [2,3,4],horizontal=True,index=1)
     # top_n = st.select_slider('Compare Top # Ngrams',[10,15,20,25],value=15)
-    top_n = st.slider("Compare Top # Ngrams", min_value=5, max_value=100, step=5,value=20)
-    use_plotly = st.checkbox("Interactive graph", value=False)
+    top_n = col2.slider("Compare Top # Ngrams", min_value=5, max_value=100, step=5,value=20)
+    use_plotly = col2.checkbox("Interactive graph", value=False)
+    col3.markdown('##### Download word cloud image.')
+    filename = col3.text_input("Filename (png)", key='flilename_mgrams',value='ngram-comparison.png')
+    download_fig_button_ngram =col3.download_button("Download image.",key='download_ngram', data =download_fig(fig),file_name=filename,mime="image/png",)
     ## Compare n-grams
+    
 ngrams_df = fn.show_ngrams(df,top_n=top_n, ngrams=ngram_n,text_col_selection=text_col_selection,stopwords_list=stopwords_list)
-
+# st.dataframe(ngrams_df)
 
 if use_plotly:
     fig = fn.plotly_group_ngrams_df(ngrams_df,show=False, title=f"Top {top_n} Most Common ngrams",width=800)
@@ -243,24 +314,97 @@ if use_plotly:
 else:
     fig = fn.plot_group_ngrams(ngrams_df,   group1_colname='Low', group2_colname="High", top_n=top_n)#,figsize=(8,12))
     st.pyplot(fig,use_container_width=True)
-# ## scattertext
-# @st.cache_data
-# def load_scattertext(fpath):
-# 	with open(fpath) as f:
-# 		explorer = f.read()
-# 		return explorer
-
-# st.subheader("ScatterText:")
-# if show_scattertext:
+    
+## add chat gpt
+chat_container = st.container()
+button_ai = st.button("Interpret with ChatGPT")
 
 
-#     # checkbox_scatter = st.checkbox("Show Scattertext Explorer",value=True)
-#     # if checkbox_scatter:
-#     with st.spinner("Loading explorer..."):
-#         html_to_show = load_scattertext(FPATHS['eda']['scattertext-by-group_html'])
-#         components.html(html_to_show, width=1200, height=800, scrolling=True)
-# else:
-#     st.empty()
+        
+    
+def get_template_string(context_low=ngrams_df.loc[:,'Low'].to_string(), context_high=ngrams_df.loc[:,'High'].to_string(), context_type='top ngrams'):
+    # task_prompt_dict = get_task_options(options_only=False)
+    # system_prompt = task_prompt_dict[selected_task]
+    template_starter = f"You are a helpful data analyst for answering questions about what customers said about a specific  Amazon product using only content from use reviews."
+    product_string = load_product_info(FPATHS['data']['app']['product-metadata-llm_json'])
+
+    product_template = f" Assume all user questions are asking about the content in the user reviews. Note the product metadata is:\n```{product_string}```\n\n"
+    template+=product_template
+    
+    # template_assistant = "You are a helpful assistant data scientist who uses NLP analysis of customer reviews to inform business-decision-making:"
+    # product_template = f" Assume all user questions are asking about the content in the user reviews. Note the product metadata is:\n```{product_string}```\n\n"
+    # template_starter = get_template_string_reviews()
+    context = f"\nGroup Contexts:\n Here is a {context_type} of 1-star reviews: ```{context_low}```.\n\n Here is a {context_type} of 5-star reviews:```{context_high}."
+    context += f"Use the {context_type} first before using the retrieved documents."
+    template_assistant=template_starter+ context
+    return template_assistant
+
+
+def get_agent(retriever=None,fpath_db=FPATHS['data']['app']['vector-db_dir'], k=8, temperature=0.1, verbose=False,
+             template_string_func=get_template_string):
+    
+    ## Make retreieval tool
+    if retriever is None:
+        retriever  = fn.load_vector_database( fpath_db,fpath_llm_csv, k=k, use_previous=True, as_retriever=True)#, use_previous=False)
+    tool = create_retriever_tool(
+        retriever,
+        "search_reviews",
+        "Searches and returns excerpts from Amazon user reviews.",
+    )
+    tools = [tool]
+    
+    
+   ## Get template via function for template string
+    template = template_string_func()
+
+
+    # Create the chatprompttemplate
+    prompt_template = OpenAIFunctionsAgent.create_prompt(
+        system_message=SystemMessage(template),
+        extra_prompt_messages=[MessagesPlaceholder(variable_name="history")],
+    )
+    
+    if verbose:
+        print(prompt_template.messages)
+        
+    llm = ChatOpenAI(temperature=temperature,streaming=True, api_key=os.getenv("OPENAI_API_KEY"))
+    agent = create_openai_tools_agent(llm, tools, prompt_template)
+    
+    ## Creating streamlit-friendly memory for streaming
+    agent_executor = AgentExecutor(agent=agent, tools=tools,  verbose=True, #return_intermediate_steps=True,
+                                   memory=ConversationBufferMemory(memory_key="history",return_messages=True))
+    return agent_executor
+
+
+            
+            
+# def reset_agent(#fpath_db = FPATHS['data']['app']['vector-db_dir'],
+#                 retriever=st.session_state['retriever'] , 
+#                 starter_message = "Hello, there! Enter your question here and I will check the full reviews database to provide you the best answer.",
+#                get_agent_kws={}):
+#     # fpath_db
+#     agent_exec = get_agent(retriever, **get_agent_kws)
+#     agent_exec.memory.chat_memory.add_ai_message(starter_message)
+#     with chat_container:
+#         st.chat_message("assistant", avatar=ai_avatar).write_stream(fake_streaming(starter_message))
+#         # print_history(agent_exec)
+#     return agent_exec
+    
+
+def fake_streaming(response):
+    import time
+    for word in response.split(" "):
+        yield word + " "
+        time.sleep(.05)		
+
+# if 'agent' not in st.session_state:
+#     # agent = get_agent(retriever)
+#     st.session_state['agent'] =get_agent(template_string_func=lambda: get_template_string(context_low=['']))# reset_agent(retriever=st.session_state['retriever'] )
+
+# if button_ai:
+#     ngrams_df = fn.show_ngrams(df,top_n=50, ngrams=ngram_n,text_col_selection=text_col_selection,stopwords_list=stopwords_list)
+#     st.session_state['agent'] = get_agent(retriever=s)
+#     response = st.session_state['agent'].invoke({'input'})
 
 
 st.sidebar.subheader("Author Information")
